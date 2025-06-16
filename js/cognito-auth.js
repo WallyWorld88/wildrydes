80c28edbd27c16d6592176af081d53629760bef6
WallyWorld88 <scott.wallace1244@gmail.com>
2025-06-16 17:03:57 -0500

    Update cognito-auth.js
    fixed user name function that removed the @ from an email

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -101,7 +101,10 @@ var WildRydes = window.WildRydes || {};
     }
 
     function toUsername(email) {
-        return email.replace('@', '-at-');
+        return email;
+        /*
+        *return email.replace('@', '-at-');
+        */
     }
 
     /*
